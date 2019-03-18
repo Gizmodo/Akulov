5403bde4f3be3bbd6e82a536505971ab06b760c4
WIN10NEARWINDOW\user <smirnovsergeyterem@gmail.com>
2019-03-18 22:50:20 +0300

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import AppNavbar from './components/AppNavbar';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <AppNavbar />
       </div>
     );
   }
