bcb1a571be6bbe1ccd9fbae2eef38e75df934760
WIN10NEARWINDOW\user <smirnovsergeyterem@gmail.com>
2019-03-19 14:58:58 +0300

    AppNavBar and Table added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
+import PrinterList from './components/PrinterList';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
         <AppNavbar />
+        <Container>
+          <PrinterList />
+        </Container>
       </div>
     );
   }
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,40 +1,44 @@
 import React, { Component } from 'react';
-import { Table } from 'reactstrap';
-class AppNavbar extends Component {
+import {
+  Collapse,
+  Container,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+class AppNavber extends Component {
+  state = {
+    isOpen: false
+  };
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
   render() {
     return (
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/Gizmodo">Github</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
     );
   }
 }
 
-export default AppNavbar;
+export default AppNavber;
