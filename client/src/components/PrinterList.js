ce71f11df2bfb0bb621ed5e97c0212a81321b966
WIN10NEARWINDOW\user <smirnovsergeyterem@gmail.com>
2019-05-17 00:20:54 +0300

    Pages count per week

--- client/src/components/PrinterList.js
+++ client/src/components/PrinterList.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { Table } from 'reactstrap';
+import { Pagination, PaginationItem, PaginationLink, Table } from 'reactstrap';
 import { getItems } from '../actions/itemActions';
 class PrinterList extends Component {
   componentDidMount = () => {
@@ -12,33 +12,77 @@ class PrinterList extends Component {
   render() {
     const { items } = this.props.item;
     return (
+
       <div>
+        <div>
+          <Pagination aria-label="Page navigation example">
+            <PaginationItem>
+              <PaginationLink first href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink previous href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">
+                1
+          </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">
+                2
+          </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">
+                3
+          </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">
+                4
+          </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">
+                5
+          </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink next href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink last href="#" />
+            </PaginationItem>
+          </Pagination>
+        </div>
         <Table>
           <thead>
             <tr>
               <th>Модель</th>
-              <th>Страницы</th>
-              <th>IP</th>
-              <th>Локация</th>
               <th>Серийный номер</th>
-              <th>Филиал</th>
               <th>Дата</th>
+              <th>Страницы</th>
+              <th>Локация</th>
+              <th>IP</th>
+              <th>Филиал</th>
+              <th>Стр. за неделю</th>
             </tr>
           </thead>
           <tbody>
-            {items.map(({ _id, model, ip, location, pages, serial, date, filial }) => (
+            {items.map(({ _id, model, ip, location, pages, serial, date, filial, diff }) => (
               <tr key={_id}>
                 <td>{model}</td>
-                <td>{pages}</td>
-                <td>
-                  <a href={'http://' + ip}>{ip}</a>
-                </td>
-                <td>{location}</td>
                 <td>{serial}</td>
-                <td>{filial}</td>
                 <td>
                   <Moment format="DD.MM.YYYY">{date}</Moment>
                 </td>
+                <td>{pages}</td>
+                <td>{location}</td>
+                <td>
+                  <a href={'http://' + ip}>{ip}</a>
+                </td>
+                <td>{filial}</td>
+                <td>{diff}</td>
               </tr>
             ))}
           </tbody>
--- routes/api/printers.js
+++ routes/api/printers.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+const moment = require('moment');
 //Printer Item Model
 const printer_item = require('../../models/printer_item');
 
 // @route  GET api/printer_item
 // @desc   Get All Printers
 // @access Public
-router.get('/', (req, res) => {
+router.get('/1', (req, res) => {
   let pipeline = [
     {
       "$lookup": {
@@ -70,4 +70,167 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/', (req, res) => {
+  var options = {
+    allowDiskUse: true
+  };
+
+  var pipeline = [
+    {
+      "$lookup": {
+        "from": "branches",
+        "let": {
+          "ip_int": "$ip_int"
+        },
+        "pipeline": [
+          {
+            "$match": {
+              "$expr": {
+                "$and": [
+                  {
+                    "$gte": [
+                      "$$ip_int",
+                      "$first_address_int"
+                    ]
+                  },
+                  {
+                    "$lte": [
+                      "$$ip_int",
+                      "$last_address_int"
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+        ],
+        "as": "Printers"
+      }
+    },
+    {
+      "$sort": {
+        "ip_int": 1.0
+      }
+    },
+    {
+      "$unwind": {
+        "path": "$Printers"
+      }
+    },
+    {
+      "$addFields": {
+        "filial": "$Printers.site_code"
+      }
+    },
+    {
+      "$project": {
+        "Printers": false
+      }
+    },
+    {
+      "$lookup": {
+        "from": "pages",
+        "localField": "serial",
+        "foreignField": "serial",
+        "as": "pg"
+      }
+    },
+    {
+      "$unwind": {
+        "path": "$pg"
+      }
+    },
+    {
+      "$match": {
+        "pg.date": {
+          "$gte": moment("2019-05-01 03:00:00.000+03:00").toDate(),
+          "$lte": moment("2019-05-30 03:00:00.000+03:00").toDate()
+        }
+      }
+    },
+    {
+      "$sort": {
+        "pg.date": -1.0
+      }
+    },
+    {
+      "$group": {
+        "_id": "$pg.serial",
+        "firstHit": {
+          "$first": "$pg.pages"
+        },
+        "lastHit": {
+          "$last": "$pg.pages"
+        },
+        "count": {
+          "$sum": 1
+        },
+        "printer_info": {
+          "$addToSet": {
+            "filial": "$filial",
+            "model": "$model",
+            "serial": "$serial",
+            "date": "$date",
+            "pages": "$pages",
+            "location": "$location",
+            "ip": "$ip",
+            "ip_int": "$ip_int"
+          }
+        }
+      }
+    },
+    {
+      "$unwind": {
+        "path": "$printer_info"
+      }
+    },
+    {
+      "$addFields": {
+        "model": "$printer_info.model",
+        "serial": "$printer_info.serial",
+        "date": "$printer_info.date",
+        "pages": "$printer_info.pages",
+        "location": "$printer_info.location",
+        "ip": "$printer_info.ip",
+        "ip_int": "$printer_info.ip_int",
+        "filial": "$printer_info.filial"
+      }
+    },
+    {
+      "$project": {
+        "printer_info": false
+      }
+    },
+    {
+      "$project": {
+        "model": true,
+        "serial": true,
+        "date": true,
+        "pages": true,
+        "location": true,
+        "ip": true,
+        "ip_int": true,
+        "filial": true,
+        "diff": {
+          "$toInt": {
+            "$subtract": [
+              "$firstHit",
+              "$lastHit"
+            ]
+          }
+        }
+      }
+    },
+    {
+      "$sort": {
+        "ip_int": 1.0
+      }
+    }
+  ];
+  printer_item
+    .aggregate(pipeline)
+    .then(items => {
+      res.json(items);
+    });
+});
 module.exports = router;
